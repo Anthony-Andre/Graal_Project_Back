6d53ca38de284b2d8547943967c4449125dbed2b
anthony.andre <anthony-andre@live.fr>
2022-12-08 14:49:08 +0100

    ADD: service delete

--- src/main/java/survey/backend/service/impl/PoeService.java
+++ src/main/java/survey/backend/service/impl/PoeService.java
@@ -6,14 +6,24 @@ import org.springframework.stereotype.Service;
 import survey.backend.entities.Poe;
 import survey.backend.repository.PoeRepository;
 
+import java.util.Optional;
+
 @Service
 public class PoeService {
 
     @Autowired
     PoeRepository repository;
 
-    public Iterable<Poe> findAll(){
+    public Iterable<Poe> findAll() {
         return this.repository.findAll();
     }
 
+    public boolean delete(int id) {
+        Optional<Poe> oPoe = this.repository.findById((long) id);
+        if (oPoe.isPresent()) {
+            this.repository.delete(oPoe.get());
+            return true;
+        }
+        return false;
+    }
 }
