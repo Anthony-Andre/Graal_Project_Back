d9dcff3817345f5cd2e6505b0beae94cf4a52bcf
RaphD <111575644+RaphD31@users.noreply.github.com>
2023-01-11 16:36:54 +0100

    sof impl QuestionServices

--- src/main/java/survey/backend/service/QuestionService.java
+++ src/main/java/survey/backend/service/QuestionService.java
@@ -1,4 +1,20 @@
 package survey.backend.service;
 
+import survey.backend.dto.QuestionDto;
+import survey.backend.dto.SurveyDto;
+
+import java.util.Collection;
+import java.util.Optional;
+
 public interface QuestionService {
+
+    Collection<SurveyDto> findAll();
+
+    Optional<SurveyDto> findById(long id);
+
+    Optional<QuestionDto> search(String title);
+    SurveyDto add(SurveyDto surveyDto);
+
+    boolean remove(long surveyId);
+
 }
--- src/main/java/survey/backend/service/impl/QuestionService.java
+++ src/main/java/survey/backend/service/impl/QuestionService.java
@@ -1,4 +1,34 @@
 package survey.backend.service.impl;
 
-public class QuestionService {
+import survey.backend.dto.QuestionDto;
+import survey.backend.dto.SurveyDto;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class QuestionService implements survey.backend.service.QuestionService {
+    @Override
+    public Collection<SurveyDto> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<SurveyDto> findById(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<QuestionDto> search(String title) {
+        return Optional.empty();
+    }
+
+    @Override
+    public SurveyDto add(SurveyDto surveyDto) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(long surveyId) {
+        return false;
+    }
 }
