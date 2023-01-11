ab49fbd1abf3193b0df3d166811f1b4aab47278d
loicgardes <loic.gardes@gmail.com>
2023-01-11 14:16:49 +0100

    Create and Set Survey Service

---
+++ src/main/java/survey/backend/service/SurveyService.java
@@ -0,0 +1,21 @@
+package survey.backend.service;
+
+import survey.backend.dto.PoeFullDto;
+import survey.backend.dto.SurveyDto;
+import survey.backend.dto.TraineeDto;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface SurveyService {
+
+    Collection<SurveyDto> findAll();
+
+    Optional<SurveyDto> findById(long id);
+
+    SurveyDto add(SurveyDto surveyDto);
+
+    Optional<SurveyDto> addQuestion(long surveyId, long questionId);
+
+    boolean remove(long surveyId);
+}
