3d2284d04f3e98049e8d63401bf39269ff53de90
loicgardes <loic.gardes@gmail.com>
2023-01-11 17:00:03 +0100

    Question Controller and DTO

--- src/main/java/survey/backend/service/impl/QuestionService.java
+++ src/main/java/survey/backend/service/impl/QuestionService.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import survey.backend.dto.QuestionDto;
 import survey.backend.repository.QuestionRepository;
+<<<<<<< HEAD
 import survey.backend.repository.entities.Question;
+=======
+>>>>>>> c2e2db5 (Question Controller and DTO)
 import survey.backend.util.StreamUtils;
 
 import java.util.Collection;
@@ -29,8 +32,12 @@ public class QuestionService implements survey.backend.service.QuestionService {
 
     @Override
     public Optional<QuestionDto> findById(long id) {
+<<<<<<< HEAD
         return this.questionRepository.findById(id)
                 .map(questionEntity -> modelMapper.map(questionEntity, QuestionDto.class));
+=======
+        return Optional.empty();
+>>>>>>> c2e2db5 (Question Controller and DTO)
     }
 
     @Override
@@ -41,12 +48,21 @@ public class QuestionService implements survey.backend.service.QuestionService {
     }
 
     @Override
+<<<<<<< HEAD
     public boolean remove(long questionId) {
         Optional<Question> oQuestion = this.questionRepository.findById(questionId);
         if (oQuestion.isPresent()) {
             this.questionRepository.delete(oQuestion.get());
             return true;
         }
+=======
+    public QuestionDto add(QuestionDto surveyDto) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(long surveyId) {
+>>>>>>> c2e2db5 (Question Controller and DTO)
         return false;
     }
 }
