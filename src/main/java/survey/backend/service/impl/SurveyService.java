1ebe2a02df35124ec374fb11a7d0f3c61ab2a616
loicgardes <loic.gardes@gmail.com>
2023-01-11 14:16:49 +0100

    Create and Set Survey Service

---
+++ src/main/java/survey/backend/service/impl/SurveyService.java
@@ -0,0 +1,45 @@
+package survey.backend.service.impl;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import survey.backend.dto.PoeDto;
+import survey.backend.dto.SurveyDto;
+import survey.backend.repository.TraineeRepository;
+import survey.backend.util.StreamUtils;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class SurveyService implements survey.backend.service.SurveyService{
+
+    //@Autowired
+    //private SurveyRepository surveyRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public Collection<SurveyDto> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<SurveyDto> findById(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public SurveyDto add(SurveyDto surveyDto) {
+        return null;
+    }
+
+    @Override
+    public Optional<SurveyDto> addQuestion(long surveyId, long questionId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean remove(long surveyId) {
+        return false;
+    }
+}
