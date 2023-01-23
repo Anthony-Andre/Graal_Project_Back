207fd97e5806228d08c84332e47ccc1b96ed9790
RaphD <111575644+RaphD31@users.noreply.github.com>
2023-01-23 10:53:44 +0100

    ajout poeInSurveys en manyTomany

--- src/main/java/survey/backend/dto/SurveyDto.java
+++ src/main/java/survey/backend/dto/SurveyDto.java
@@ -5,13 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import survey.backend.repository.entities.Poe;
 import survey.backend.repository.entities.Question;
 import survey.backend.dto.enums.Level;
 import survey.backend.dto.enums.PoeType;
 
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 @SuperBuilder
 @NoArgsConstructor
@@ -23,6 +22,7 @@ public class SurveyDto {
     private String title;
     private Level level;
     private PoeType poeType;
-    private LinkedList<Question> questions;
+    private List<Question> questions;
+    private List<Poe> poes;
 
 }
